0cdc40ff7fba2c9abfd44995e500d767f82f09c0
Werefox <jack@wrightus.com>
2022-05-08 19:25:51 -0400

    Added places index page view

---
+++ views/places/index.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function index() {
+	return (
+		<html>
+			<main>
+				<h1>Places index</h1>
+			</main>
+		</html>
+	)
+}
+
+module.exports = index
