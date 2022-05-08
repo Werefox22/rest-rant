6a2dd9d401ae8a359ad2ba938e047ba0e4da4220
Werefox <jack@wrightus.com>
2022-05-08 19:30:54 -0400

    Added mock data to the places view

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,11 +1,23 @@
 const React = require('react')
 const Def = require('../default')
 
-function index() {
+function index(data) {
+	// format data
+	let placesFormatted = data.places.map((place) => {
+		return (
+			<div>
+				<h2>{place.name}</h2>
+				<img src={place.pic} alt={place.name}></img>
+			</div>
+		)
+	})
+
+	// display page
 	return (
 		<html>
 			<main>
 				<h1>Places index</h1>
+				{placesFormatted}
 			</main>
 		</html>
 	)
