846260679b1acb6b61b69847e57f2b8e4b6c0702
Werefox <jack@wrightus.com>
2022-05-08 19:16:26 -0400

    Added home page view

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// view engine
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// controller
 app.use('/places', require('./controllers/places'))
 
+// routes
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
+// 404 page
 app.get('*', (req, res) => {
 	res.status(404).send("<h1>404 Page</h1>")
 })
 
+// listen
 app.listen(process.env.PORT)
