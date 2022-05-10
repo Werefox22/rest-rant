6eabca8a91ac1fc1e980d73b8df2533f226dc5ee
Werefox <jack@wrightus.com>
2022-05-09 21:07:50 -0400

    Added a photo to the 404 page and some image styling to the css file

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,13 @@ function error404() {
 		<Default>
 			<main>
 				<h1>404: Page not found</h1>
+				<p>Whoops, sorry! We couldn't find that page!</p>
+				<div>
+					<img src="images/cat.jpg" alt="A cat hiding under a blanket" />
+					<div>
+						Photo by <a href="https://unsplash.com/@miklevasilyev?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mikhail Vasilyev</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+					</div>
+				</div>
 			</main>
 		</Default>
 	)
