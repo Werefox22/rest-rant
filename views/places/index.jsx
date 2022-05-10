79ba290556dadbe61bcaa4a1544e715a8c0e3216
Werefox <jack@wrightus.com>
2022-05-09 21:17:51 -0400

    Added bootstrap grid to index page and displayed the rest of the places data

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,9 +5,11 @@ function index(data) {
 	// format data
 	let placesFormatted = data.places.map((place) => {
 		return (
-			<div>
+			<div className='col-sm-6'>
 				<h2>{place.name}</h2>
 				<img src={place.pic} alt={place.name}></img>
+				<p>{place.cuisines}</p>
+				<p>Located in {place.city}, {place.state}</p>
 			</div>
 		)
 	})
@@ -16,8 +18,10 @@ function index(data) {
 	return (
 		<Default>
 			<main>
-				<h1>Places index</h1>
-				{placesFormatted}
+				<h1>Places to Rant or Rave about</h1>
+				<div className='row'>
+					{placesFormatted}
+				</div>
 			</main>
 		</Default>
 	)
