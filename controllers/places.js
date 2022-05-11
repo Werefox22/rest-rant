faad6a0b2bd90f3f97cc4f676683956fd12f07aa
Werefox <jack@wrightus.com>
2022-05-10 21:58:30 -0400

    Moved the places array to its own file

--- controllers/places.js
+++ controllers/places.js
@@ -1,22 +1,20 @@
 const router = require('express').Router()
+const Places = require('../models/places.js')
 
+// routes
+// /places
 router.get('/', (req, res) => {
-	let places = [
-		{
-			name: 'H-Thai-ML',
-			city: 'Seattle',
-			state: 'WA',
-			cuisines: 'Thai, Pan-Asian',
-			pic: 'images/thai food.jpg'
-		}, {
-			name: 'Coding Cat Cafe',
-			city: 'Phoenix',
-			state: 'AZ',
-			cuisines: 'Coffee, Bakery',
-			pic: 'images/coffeecat.jpg'
-		}]
+	res.render('places/index', {Places})
+})
 
-	res.render('places/index', {places})
+// /places/new
+router.get('/new', (req, res) => {
+	res.render('places/new')
+})
+
+// places/:id
+router.get('/:id', (req, res) => {
+	res.send(Places[req.params.id])
 })
 
 module.exports = router
