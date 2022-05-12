9705b4f8c2e8d5b510c6d7fccdbe2c2a661e4e42
Werefox <jack@wrightus.com>
2022-05-11 20:14:15 -0400

    Added edit and delete buttons to the show view

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 // view engine
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 // controller
 app.use('/places', require('./controllers/places_controller'))
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,14 +5,32 @@ function show(data) {
 	return (
 		<Default>
 			<main>
-				<h2>{data.place.name}</h2>
-				<img src={data.place.pic} alt={data.place.name} />
-				<h2>Rating</h2>
-				<p>Not yet rated</p>
-				<h4>Cuisines</h4> 
-				<p>{data.place.cuisines}</p>
-				<h4>Location</h4>
-				<p>{`${data.place.city}, ${data.place.state}`}</p>
+				<div className='row'>
+
+					<div className='col-sm-6'>
+						<h2>{data.place.name}</h2>
+						<img src={data.place.pic} alt={data.place.name} />
+
+						<div className='buttons'>
+							<a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
+							<form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+								<button type='submit' className='btn btn-danger'>
+									Delete
+								</button>
+							</form>
+						</div>
+					</div>
+
+					<div className='col-sm-6'>
+						<h2>Rating</h2>
+						<p>Not yet rated</p>
+						<h4>Cuisines</h4> 
+						<p>{data.place.cuisines}</p>
+						<h4>Location</h4>
+						<p>{`${data.place.city}, ${data.place.state}`}</p>
+					</div>
+				</div>
+
 				<hr />
 				<h3>Comments</h3>
 				<p>No comments yet!</p>
