8ff9ba23b6c1b2bb0c4de46978c2a791ab6b4660
Werefox <jack@wrightus.com>
2022-05-11 20:03:15 -0400

    Filled out the show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,21 @@
 const React = require('react')
 const Default = require('../default')
 
-function show() {
+function show(data) {
 	return (
 		<Default>
 			<main>
-				<h2>Show Page</h2>
+				<h2>{data.place.name}</h2>
+				<img src={data.place.pic} alt={data.place.name} />
+				<h2>Rating</h2>
+				<p>Not yet rated</p>
+				<h4>Cuisines</h4> 
+				<p>{data.place.cuisines}</p>
+				<h4>Location</h4>
+				<p>{`${data.place.city}, ${data.place.state}`}</p>
+				<hr />
+				<h3>Comments</h3>
+				<p>No comments yet!</p>
 			</main>
 		</Default>
 	)
