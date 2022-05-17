eca2fdf379f5bd21e1c95aa22c97b5b07de94f07
Werefox <jack@wrightus.com>
2022-05-16 20:27:04 -0400

    Installed mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // view engine
 app.set('view engine', 'jsx')
@@ -9,6 +10,12 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+module.exports.Place = require('./places')
 
 // controller
 app.use('/places', require('./controllers/places_controller'))
