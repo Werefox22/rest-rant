d9c44e00cbabd8f9a820cd942bdcfefd9c2f258b
Werefox <jack@wrightus.com>
2022-05-18 21:42:35 -0400

    Updated the seeding script to generate random places

--- seeders/seed-places.js
+++ seeders/seed-places.js
@@ -1,20 +1,28 @@
 const db = require('../models')
 
-db.Place.create([{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg',
-    founded: 1989
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg',
-    founded: 2020
-}])
+// fake names
+let nouns = [ "Tony's", "Bobs's", "The Red", "Royal", "Best", "Famous" ]
+let rests = [ "Place", "Shack", "Grill", "Breakfast", "House", "Ovens", "Kitchen" ]
+let cities = [ "Seattle", "Charlotte", "Raleigh", "Durham", "Atlanta", "Pheonix", "Birmingham", "Detroit" ]
+let states = [ "WA", "AZ", "AL", "MI", "NC", "SC", "GA" ]
+let food = [ "Coffee", "Pizza", "Mexican", "Italian", "American", "Thai", "Bakery" ]
+
+// generate random places
+let newPlaces = []
+for (let i = 0; i < 4; i++) {
+    let place = {
+        name: nouns[Math.floor(Math.random() * nouns.length)] + " " + rests[Math.floor(Math.random() * rests.length)],
+        city: cities[Math.floor(Math.random() * cities.length)],
+        state: states[Math.floor(Math.random() * states.length)],
+        cuisines: food[Math.floor(Math.random() * food.length)],
+        pic: undefined,
+        founded: Math.floor(Math.random() * 100) + 1922
+    }
+    newPlaces.push(place)
+}
+
+// create them
+db.Place.create(newPlaces)
 .then(() => {
 	console.log("Seeding was successful")
 	process.exit()
